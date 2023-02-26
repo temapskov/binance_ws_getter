35d0112b3378dc5839e82130fbc7188ce4b25758
temapskov <temapskov@yandex.ru>
2023-02-26 23:56:03 +0300

    Initial commit
    Signed-off-by: temapskov <temapskov@yandex.ru>

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # binance_ws_getter
- Get data from Binance with WebSockets
+
+Скрипт, который сначала записывает время в микросекундах (`receive_ts`), затем получает [данные](https://binance-docs.github.io/apidocs/futures/en/#diff-book-depth-streams) по WebSockets от Binance и забирает от туда время `T` (`transaction_time`). Сохраняет строку в CSV файл, строка,
+
+`receive_ts;transaction_time;interval`, где interval - это `receive_ts` - `transaction_time`.
+
+## Установка
+```
+pip install -r requirements.txt
+```
---
+++ main.py
@@ -0,0 +1,36 @@
+import asyncio
+import aiofiles
+import websockets
+import json
+import time
+from aiocsv import AsyncWriter
+
+SOCKET = "wss://fstream.binance.com/stream?streams=btcusdt@depth"
+OUTPUT_FILENAME = "output.csv"
+
+
+async def write_file(receive_ts: int, transaction_time: int):
+    interval = 0
+
+    async with aiofiles.open(OUTPUT_FILENAME, "a") as csvfile:
+        writer = AsyncWriter(csvfile, delimiter=";")
+
+        if transaction_time:
+            interval = receive_ts - transaction_time
+            await writer.writerow([receive_ts, transaction_time, interval])
+        else:
+            await writer.writerow(["Ошибка получения данных"])
+
+
+async def main():
+    while True:
+        async with websockets.connect(SOCKET) as websocket:
+            receive_ts = int(time.time() * 1000)
+            msg = await websocket.recv()
+            js = json.loads(msg)
+            transaction_time = js.get("data").get("T")
+
+            await write_file(receive_ts, transaction_time)
+
+
+asyncio.run(main())
